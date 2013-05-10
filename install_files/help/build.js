d3f9deb2038e17590be154f3416124fe0267b366
Barbara Geller <barbara@copperspice.com>
2013-05-09 18:16:25 -0800

    Update make files

---
+++ install_files/help/build.js
@@ -0,0 +1,7 @@
+var build =
+[
+    [ "Building for X11", "require-x11.html", null ],
+    [ "Building for Windows", "require-win.html", null ],
+    [ "Building for OS X", "require-mac.html", null ],
+    [ "Compile & Link", "build-diamond.html", null ]
+];
